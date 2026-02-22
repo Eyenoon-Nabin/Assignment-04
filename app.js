208d2d099141b301cd76db2e7efbda251c30b249
Eyenoon-Nabin <eyenoon28@gmail.com>
2026-02-23 00:49:52 +0600

    add java again

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-// Shonkha Dekhanor jayga dhoelam
+// 1 element dhorlam
 const interviewCountElement = document.getElementById('interview-count');
 const rejectedCountElement = document.getElementById('rejected-count');
+const emptyState = document.getElementById('empty-state');
+const allCards = document.querySelectorAll('.job-card');
+
+//tab button dhorlam
+const tabAll = document.getElementById('tab-all');
+const tabInterview = document.getElementById('tab-interview');
+const tabRejected = document.getElementById('tab-rejected');
 
 let interviewCount = 0;
 let rejectedCount = 0;
 
-//Interview & Rejected
 const interviewButtons = document.querySelectorAll('.btn-success');
 const rejectedButtons = document.querySelectorAll('.btn-error');
-
-// ৩. Interview button o Event Listener
-for (let btn of interviewButtons) {
-    btn.addEventListener('click', function () {
-        interviewCount++;
-        interviewCountElement.innerText = interviewCount;
-    });
-}
-
-//Rejected button o Event Listener
-for (let btn of rejectedButtons) {
-    btn.addEventListener('click', function () {
-        rejectedCount++;
-        rejectedCountElement.innerText = rejectedCount;
-    });
-}
